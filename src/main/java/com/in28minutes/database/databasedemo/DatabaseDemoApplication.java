530e4c6f37f2420dbf618492e534263eee768989
Vimosanan <vimosanan.13@cse.mrt.ac.lk>
2018-04-09 17:56:37 +0530

    insert, update query files

--- src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
+++ src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
@@ -1,12 +1,39 @@
 package com.in28minutes.database.databasedemo;
 
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.in28minutes.database.databasedemo.entity.Person;
+import com.in28minutes.database.databasedemo.jdbc.PersonJdbcDao;
+
 @SpringBootApplication
-public class DatabaseDemoApplication {
+public class DatabaseDemoApplication implements CommandLineRunner {
+
+	@Autowired
+	PersonJdbcDao dao;
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("All user -> {}", dao.findAll());
+		logger.info("User deatails of id-10001 is {}", dao.findById(10001));
+		logger.info("Deleting user of Id-10002, no of rows affected {}", dao.deleteById(10002));
+
+		logger.info("Inserting user of id 10004, no of rows affected - {}",
+				dao.insert(new Person(10004, "Sanga", "Sydney", new Date())));
+
+		logger.info("updating user of id 10001, no of rows affected - {}",
+				dao.update(new Person(10001, "Vimo", "Colombo", new Date())));
 	}
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,20 @@
-create table person(
+CREATE table person(
 	id integer not null,
 	name varchar(255) not null,
 	location varchar(255),
 	birth_date timestamp,
 	primary key(id)
 );
+
+INSERT INTO person(
+	id, name, location, birth_date) 
+	VALUES( 10001, 'Hearth','Matale', sysdate());
+	
+INSERT INTO person(
+	id, name, location, birth_date) 
+	VALUES( 10002, 'Mendis','Kandy', sysdate());
+	
+INSERT INTO person(
+	id, name, location, birth_date) 
+	VALUES( 10003, 'Malinga','Kurunagal', sysdate());
+	
